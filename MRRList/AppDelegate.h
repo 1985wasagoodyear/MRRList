d1bd68aa9f0d19e5dcdfc0879b29ace1ebf619df
1985wasagoodyear <1985wasagoodyear>
2019-09-17 15:00:42 -0400

    Initial Commit

---
+++ MRRList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MRRList
+//
+//  Created by K Y on 9/17/19.
+//  Copyright © 2019 K Y. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MRRList/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MRRList
+//
+//  Created by K Y on 9/17/19.
+//  Copyright © 2019 K Y. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
